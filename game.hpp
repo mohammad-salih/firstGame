d452806eb7cac85a1d20a2a20c94dc032ff1c7c3
mohammad-salih <34977827+mohammad-salih@users.noreply.github.com>
2018-01-31 14:51:09 +0300

    Create game.hpp

---
+++ game.hpp
@@ -0,0 +1,12 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#define GAME_API
+
+typedef void(void)      GameThread
+
+class GAME_API Game{
+                GameThread;
+        public:
+                
+}
